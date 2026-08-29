c325dfe0ff264604a26190794986a62b76bcf2ea
Zach Garrett <zach@very.coffee>
2026-08-29 12:43:55 -0400

    remove blog and bun-web packages

--- packages/blog/src/node/index.ts
+++
@@ -1,3 +0,0 @@
-export { compilePostToTsx } from "./compile-post";
-export { type GenerateManifestOptions, generateManifest } from "./generate-manifest";
-export { loadPostFiles, type PostFileEntry } from "./load-post-files";
